422252e4993a1707cbabb0f123c545a0f9bf320e
Rafał Koguciuk <rkoguciu@gepard.robotyka.ia.pw.edu.pl>
2017-04-10 11:42:50 +0200

    Lab2

--- anrobot/include/anrobot/simple_fwd_kinematics.h
+++
@@ -1,7 +0,0 @@
-#include <math.h>
-#include <iostream>
-#include <ros/ros.h>
-#include "anrobot/FwdKinematics.h"
-
-bool get_pose(anrobot::FwdKinematics::Request  &req,
-	      anrobot::FwdKinematics::Response &res);
